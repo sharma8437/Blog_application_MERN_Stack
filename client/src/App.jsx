175c331e0611909c111cc92101d6e23ff58af3de
nikesh-avua <nikesh.sharma@avua.com>
2025-08-20 00:30:23 +0530

    created the other components footer login dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Blog from './components/pages/Blog'
+import Layout from './components/pages/admin/Layout'
+import Dashboard from './components/pages/admin/Dashboard'
+import AddBlog from './components/pages/admin/AddBlog'
+import ListBlog from './components/pages/admin/ListBlog'
+import Comments from './components/pages/admin/Comments'
+import Login from './components/admin/Login'
 
 const App = () => {
   return (
@@ -9,6 +15,14 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/blog/:id' element={<Blog />} />
+
+        <Route path='/admin' element={ true ? <Layout /> : <Login />}>
+        <Route  index element={<Dashboard />} />
+        <Route  path='addBlog' element={<AddBlog />} />
+        <Route  path='listBlog' element={<ListBlog />} />
+        <Route  path='comments' element={<Comments />} />
+
+        </Route>
         </Routes> 
     </div>
   )
